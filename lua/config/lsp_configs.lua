6cbd39b5c9dc00677b9ee0e0dfeec78cafbad0f7
João Alves <njoao97710@gmail.com>
2024-06-01 01:02:48 -0300

    refactor: aprimora configuracao do lsp

--- lua/config/lsp_configs.lua
+++ lua/config/lsp_configs.lua
@@ -1,31 +1,13 @@
 require("mason").setup()
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
 local mason_lspconfig = require("mason-lspconfig")
 local ruby_lsp_config = require("util.ruby-lsp")
-local coq = require("coq")
-
-vim.cmd("COQnow -s")
 
 local servers = {
   lua_ls = {},
-  ruby_lsp = {
-    settings = {
-      formatter = "rubocop",
-    },
-  },
+  ruby_lsp = {},
 }
 
 mason_lspconfig.setup({
   ensure_installed = vim.tbl_keys(servers),
-})
-
-mason_lspconfig.setup_handlers({
-  function(server_name)
-    require("lspconfig")[server_name].setup(coq.lsp_ensure_capabilities({
-      on_attach = function(client, buffer)
-        ruby_lsp_config.on_attach(client, buffer)
-      end,
-    }))
-  end,
 })
---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,23 @@
+return {
+  "neovim/nvim-lspconfig",
+  lazy = false,
+  dependencies = {
+    { "ms-jpq/coq_nvim", branch = "coq" },
+    { "ms-jpq/coq.artifacts", branch = "artifacts" },
+  },
+  init = function()
+    vim.g.coq_settings = {
+      auto_start = true,
+    }
+  end,
+  config = function()
+    local lsp = require("lspconfig")
+    local ruby_lspconfig = require("util.ruby-lsp")
+
+    lsp.ruby_lsp.setup({
+      on_attach = function(client, buffer)
+        ruby_lspconfig.on_attach(client, buffer)
+      end,
+    })
+  end,
+}
