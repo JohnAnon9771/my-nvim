70b81b6f03454df1611319b21ad9828f274f4a51
JohnAnon9771 <njoao97710@gmail.com>
2025-11-23 22:30:07 -0300

    fix: corrige carregamento de keymaps e plugins

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.options")
 require("config.lazy")
 require("config.keymaps")
-require("config.options")
 
 -- Note: opções, keymaps e autocmds são carregados automaticamente pelo LazyVim
 -- ou pelo require("config.lazy") que importa os specs.
